b881621992036cf024e848edeeb80edaba86ae90
fsbalbuena <fsbalbuena@gmail.com>
2019-05-04 18:31:54 -0300

    first commit

---
+++ DesarrollosPyme.github.io/index.js
@@ -0,0 +1,27 @@
+$('.carousel').carousel({
+  interval: 7000
+})
+var array=[{id:"#ce",url:"#pepe"},
+{id:"#bfe",url:"https://www.felizemprendiendo.com"},
+{id:"#trabajosRealizados",url:"https://demos.desarrollospyme.com/pw-trabajos.html"},
+{id:"#nos-l",url:"#nos"},
+{id:"#dise",url:"https://demos.desarrollospyme.com/pw-trabajos.html?d=2"},
+{id:"#face",url:"#"},
+{id:"#insta",url:"#"},
+,{id:"#pwi",url:"https://www.desarrollospyme.com/paginas-web#Paginas-institucionales"},
+{id:"#pw24",url:"https://www.desarrollospyme.com/paginas-web#Paginas-24hs"},
+{id:"#gM",url:"https://www.desarrollospyme.com/paginas-web#GoogleMaps"},
+{id:"#price",url:"https://www.desarrollospyme.com/paginas-web#Precios"}]
+
+array.forEach(element => {
+    
+    $(element.id).on("click",function(e){
+        e.stopPropagation()
+        window.location=element.url
+    }) 
+});
+$("#coti").on("click",function(e){
+    e.stopPropagation()
+    $("#coti").css("display","none")
+    $("#cotizar-form").css("display","block")
+})
